0e7cd5fe874f77d09db96cd3b51ea507ced15d72
odyg <odygaro@gmail.com>
2023-04-30 21:24:29 -0600

    added more sounds

--- index.js
+++ index.js
@@ -3,6 +3,42 @@ for (let i = 0; i < drumList.length; i++) {
   drumList[i].addEventListener("click", handleClick);
 }
 function handleClick() {
-  var audio = new Audio("sounds/tom-1.mp3");
-  audio.play();
+  var buttonInnerHTML = this.innerHTML;
+
+  switch (buttonInnerHTML) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom1 = new Audio("sounds/tom-2.mp3");
+      tom1.play();
+      break;
+    case "s":
+      var tom1 = new Audio("sounds/tom-3.mp3");
+      tom1.play();
+      break;
+    case "d":
+      var tom1 = new Audio("sounds/tom-4.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom1 = new Audio("sounds/snare.mp3");
+      tom1.play();
+      break;
+    case "k":
+      var tom1 = new Audio("sounds/crash.mp3");
+      tom1.play();
+      break;
+    case "l":
+      var tom1 = new Audio("sounds/kick-bass.mp3");
+      tom1.play();
+      break;
+    default:
+      console.log(buttonInnerHTML);
+      break;
+  }
 }
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
